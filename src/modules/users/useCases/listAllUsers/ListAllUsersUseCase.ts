fef3f40c59872ac1ba8566b279a390594c043fed
henrique2m <btdavintehenrique@gmail.com>
2021-04-21 20:14:14 -0300

    chore: :sparkles: challenge[OK] :rocket:

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,21 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!userIsAdmin) {
+      throw new Error("User not found.");
+    }
+
+    const { admin } = userIsAdmin;
+
+    if (!admin) {
+      throw new Error("Access denied.");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
